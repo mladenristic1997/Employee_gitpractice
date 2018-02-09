fe6260e6ad2ab301d6cdfee926fc9fc67fa7d726
Mladen Ristic <mladenristic1997@gmail.com>
2018-02-09 13:19:32 +0100

    person done

--- Person.java
+++ Person.java
@@ -1,5 +1,57 @@
 package person_classes;
+/*
+ * name
+ * address
+ * phone number
+ * email address
+ */
+
 
 public class Person {
+	
+	private String name;
+	private String address;
+	private String phoneNumber;
+	private String mail;
+	
+	public Person(String name, String address, String phoneNumber, String mail){
+		this.name = name;
+		this.address = address;
+		this.phoneNumber = phoneNumber;
+		this.mail = mail;
+	}
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getMail() {
+		return mail;
+	}
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+	
+	
 }
