25375752041cf2f45a08976bc1960aeddbfaebb7
Zeljko <zeljkod93@live.com>
2018-02-09 13:56:08 +0100

    Staff klasa

--- person_classes/Staff.java
+++ person_classes/Staff.java
@@ -4,5 +4,38 @@ package person_classes;
  * has title
  */
 public class Staff {
+	
+	private String title;
+	private String name;
+	
+	
+public Staff() {
+	
+}
+
+public Staff(String title, String name) {
+	this.title = title;
+	this.name = name;
+}
+
+public String getTitle() {
+	return title;
+}
+
+public void setTitle(String title) {
+	this.title = title;
+}
+
+public String getName() {
+	return name;
+}
+
+public void setName(String name) {
+	this.name = name;
+}
+
+public String toString(){
+	return "Staff class " + Staff.class + " title: " + getTitle() + " Staff name: " + getName();
+}
 
 }
