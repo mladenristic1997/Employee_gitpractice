4eff4c84f26ebf5d519de6949d3529dcb035fdb3
Mladen Ristic <mladenristic1997@gmail.com>
2018-02-12 14:12:02 +0100

    added student

--- Student.java
+++ Student.java
@@ -3,6 +3,24 @@ package person_classes;
  * extends person
  * class status (freshman, sophomore, junior, or senior) status je konstanta
  */
-public class Student {
+public class Student extends Person{
+	
+	final int FRESHMAN = 1;
+	final int SOPHOMORE = 2;
+	final int JUNIOR = 3;
+	final int SENIOR = 4;
 
+	int year;
+	public Student(String name, String address, String phoneNumber, String mail, int year) {
+		super(name, address, phoneNumber, mail);
+		this.year = year;
+	}
+	
+	public int getYear() {
+		return year;
+	}
+	public void setYear(int year) {
+		this.year = year;
+	}
+		
 }
